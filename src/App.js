5e254d3343c1d7840024940f59200c1a2708feda
Pranay Aswani <v-pranay.aswani@yugen.ai>
2026-04-14 12:58:52 +0530

    useState when state is a object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [person, setPerson] = useState({age:19,siblings:20});
+ 
+
+  const handleAge = (age)=>{
+      setPerson({...person,age:age+1})
+  }
+
+  const handleSiblings = (siblings)=>{
+      setPerson({...person,siblings:siblings+1})
+  }
+
   return (
     <div className="App">
-      <h1>Pranay Aswani</h1>
-      <p>I am getting ready for my Marriage</p>
-      <button>Say Hello</button>
+      <p>Today I am {person.age} Years Old</p>
+      <p>I have {person.siblings} Siblings</p>
+      <button onClick={()=>handleAge(person.age)}>Get Older</button>
+      <button onClick={()=>handleSiblings(person.siblings)}>More Siblings</button>
     </div>
   );
 }
